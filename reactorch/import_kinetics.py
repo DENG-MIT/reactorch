5c8b226446c0dcda5008b2f1139a7aef62c6f6aa
ZhiyuShi <66129026+ZhiyuShi@users.noreply.github.com>
2020-07-06 00:09:53 +0800

    Update import_kinetics.py

--- reactorch/import_kinetics.py
+++ reactorch/import_kinetics.py
@@ -15,6 +15,8 @@ def set_nasa(self):
 def set_reactions(self):
 
         self.reaction = [[None]] * self.n_reactions
+        
+        self.n_rate_constants = [[None]] * self.n_reactions
 
         self.reactant_stoich_coeffs = torch.Tensor(self.gas.reactant_stoich_coeffs()).to(self.device)
 
@@ -43,13 +45,14 @@ def set_reactions(self):
         self.list_reaction_type4_Troe = []
         # the id of Troe in type 4
         self.list_id_Troe_in_type4 = []
+        self.list_reaction_type5 = []
         
         # count the num in type 4
         count_type4 = -1
 
         for i in range(self.n_reactions):
 
-            # Type 1: regular reaction, 2: three-body, 4:fall-off
+            # Type 1: regular reaction, 2: three-body, 4:fall-off, 5:pressure-dependent-Arrhenius
 
             yaml_reaction = self.model_yaml['reactions'][i]
 
@@ -68,6 +71,9 @@ def set_reactions(self):
                 # id of the current item in the list
                 count_type4 = count_type4 + 1
                 self.list_reaction_type4.append(i)
+                
+            if self.gas.reaction_type(i) in [5]:
+                self.list_reaction_type5.append(i)
 
             if self.gas.is_reversible(i) is False:
                 self.is_reversible[i].fill_(0)
@@ -165,6 +171,52 @@ def set_reactions(self):
                                                     'T2': torch.Tensor([1e30]).to(self.device),
                                                     'T3': torch.Tensor([Troe['T3']]).to(self.device)
                                                     }
+                        
+            if self.gas.reaction_type(i) in [5]:
+                self.n_rate_constants[i] = len(self.gas.reaction(i).rates)
+                self.reaction[i]['p_dep'] = {}
+                self.reaction[i]['p_dep']['A'] = [[None]] * self.n_rate_constants[i]
+                self.reaction[i]['P'] = [[None]] * self.n_rate_constants[i]
+                self.reaction[i]['b'] = [[None]] * self.n_rate_constants[i]
+                self.reaction[i]['Ea'] = [[None]] * self.n_rate_constants[i]
+
+                for j in range(self.n_rate_constants[i]):
+                    pdep_arrhenius = yaml_reaction['rate-constants'][j]
+
+                    if type(pdep_arrhenius['P']) is str:
+                        P = list(map(eval, [pdep_arrhenius['P'].split(' ')[0]]))
+                        self.reaction[i]['P'][j] = torch.Tensor(P).to(self.device)
+                        if [pdep_arrhenius['P'].split(' ')[1]] == ['atm'] or [pdep_arrhenius['P'].split(' ')[1]] == ['ATM']:
+                            self.reaction[i]['P'][j] = 101325 * self.reaction[i]['P'][j]
+                        if [pdep_arrhenius['P'].split(' ')[1]] == ['MPa']:
+                            self.reaction[i]['P'][j] = 1000000 * self.reaction[i]['P'][j]
+                    else:
+                        P = [pdep_arrhenius['P']]
+                        self.reaction[i]['P'][j] = torch.Tensor(P).to(self.device)
+
+                    if type(pdep_arrhenius['A']) is str:
+                        A = list(map(eval, [pdep_arrhenius['A'].split(' ')[0]]))
+                    else:
+                        A = [pdep_arrhenius['A']]
+                    self.reaction[i]['p_dep']['A'][j] = torch.Tensor(A).to(self.device)
+
+                    if type(pdep_arrhenius['b']) is str:
+                        b = list(map(eval, [pdep_arrhenius['b'].split(' ')[0]]))
+                    else:
+                        b = [pdep_arrhenius['b']]
+                    self.reaction[i]['b'][j] = torch.Tensor(b).to(self.device)
+
+                    if type(pdep_arrhenius['Ea']) is str:
+                        Ea = list(map(eval, [pdep_arrhenius['Ea'].split(' ')[0]]))
+                    else:
+                        Ea = [pdep_arrhenius['Ea']]
+                    self.reaction[i]['Ea'][j] = torch.Tensor(Ea).to(self.device)
+
+                if type(pdep_arrhenius['A']) is str:
+                    A = list(map(eval, [pdep_arrhenius['A'].split(' ')[0]]))
+                else:
+                    A = [pdep_arrhenius['A']]
+                self.reaction[i]['A'] = torch.Tensor(A).to(self.device)
 
             if 'orders' in yaml_reaction:
 
@@ -175,7 +227,8 @@ def set_reactions(self):
             if 'units' in self.model_yaml:
 
                 if self.model_yaml['units']['length'] == 'cm' and self.model_yaml['units']['quantity'] == 'mol':
-                    self.reaction[i]['A'] *= (1e-3) ** (self.reactant_stoich_coeffs[:, i].sum().item() - 1)
+                    if self.gas.reaction_type(i) in [1, 2, 4]:
+                        self.reaction[i]['A'] *= (1e-3) ** (self.reactant_stoich_coeffs[:, i].sum().item() - 1)
 
                     if self.gas.reaction_type(i) in [2]:
                         self.reaction[i]['A'] *= 1e-3
@@ -183,10 +236,20 @@ def set_reactions(self):
                     if self.gas.reaction_type(i) in [4]:
                         self.reaction[i]['A_0'] *= 1e-3
                         self.reaction[i]['A_0'] *= (1e-3) ** (self.reactant_stoich_coeffs[:, i].sum().item() - 1) 
+                        
+                    if self.gas.reaction_type(i) in [5]:
+                        for j in range(self.n_rate_constants[i]):
+                            self.reaction[i]['p_dep']['A'][j] *= 1e-3 ** (self.reactant_stoich_coeffs[:, i].sum().item() - 1)
 
-            self.Arrhenius_coeffs[i, 0] = self.reaction[i]['A']
-            self.Arrhenius_coeffs[i, 1] = self.reaction[i]['b']
-            self.Arrhenius_coeffs[i, 2] = self.reaction[i]['Ea']
+            if self.gas.reaction_type(i) in [1, 2, 4]:
+                self.Arrhenius_coeffs[i, 0] = self.reaction[i]['A']
+                self.Arrhenius_coeffs[i, 1] = self.reaction[i]['b']
+                self.Arrhenius_coeffs[i, 2] = self.reaction[i]['Ea']
+
+            if self.gas.reaction_type(i) in [5]:
+                self.Arrhenius_coeffs[i, 0] = self.reaction[i]['A']
+                self.Arrhenius_coeffs[i, 1] = self.reaction[i]['b'][0]
+                self.Arrhenius_coeffs[i, 2] = self.reaction[i]['Ea'][0]
 
         self.Arrhenius_A = self.Arrhenius_coeffs[:, 0]
         self.Arrhenius_b = self.Arrhenius_coeffs[:, 1]
