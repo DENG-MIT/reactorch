11135e9de8ce6421a917553b4275128e6f45a746
ZhiyuShi <66129026+ZhiyuShi@users.noreply.github.com>
2020-07-11 11:34:01 +0800

    Update flame_1d.py

--- reactorch/flame_1d.py
+++ reactorch/flame_1d.py
@@ -15,7 +15,7 @@ torch.set_default_tensor_type("torch.DoubleTensor")
 
 
 class Flame_1d:
-    def __init__(self, tct, device=None):
+    def __init__(self, sol, device=None):
         super().__init__()
         
         if device is None:
@@ -23,11 +23,11 @@ class Flame_1d:
         else:
             self.device = device
         
-        self.tct = tct
+        self.sol = sol
 
     def set_states(self, TPY):
 
-        self.tct.set_states(TPY)
+        self.sol.set_states(TPY)
 
         self.update_u()
         self.update_diffusive_fluxes()
@@ -42,7 +42,7 @@ class Flame_1d:
         self.m_dot = torch.Tensor([m_dot]).to(self.device)
 
     def update_u(self):
-        self.u = self.m_dot / self.tct.density_mass
+        self.u = self.m_dot / self.sol.density_mass
 
     def grad_to_x(self, output):
 
@@ -66,25 +66,25 @@ class Flame_1d:
 
     def update_diffusive_fluxes(self):
         # following https://cantera.org/science/flames.html
-        self.tct.update_transport()
-        self.diffusive_fluxes_star = self.tct.density_mass * \
-            (self.tct.molecular_weights.T / self.tct.mean_molecular_weight) \
-            * self.tct.mix_diff_coeffs * self.grad_to_x(self.tct.X)
+        self.sol.update_transport()
+        self.diffusive_fluxes_star = self.sol.density_mass * \
+            (self.sol.molecular_weights.T / self.sol.mean_molecular_weight) \
+            * self.sol.mix_diff_coeffs * self.grad_to_x(self.sol.X)
         self.diffusive_fluxes = self.diffusive_fluxes_star - \
-            self.tct.Y.clone() * self.diffusive_fluxes_star.sum(dim=1, keepdim=True)
+            self.sol.Y.clone() * self.diffusive_fluxes_star.sum(dim=1, keepdim=True)
 
     def update_dYdx(self):
         # following https://cantera.org/science/flames.html
-        self.dYdx = - (self.grad_to_x(self.diffusive_fluxes) + self.tct.molecular_weights.T * self.tct.wdot) / self.m_dot
+        self.dYdx = - (self.grad_to_x(self.diffusive_fluxes) + self.sol.molecular_weights.T * self.sol.wdot) / self.m_dot
 
     def update_dTdx(self):
         # following https://cantera.org/science/flames.html
-        self.dT2dx = self.grad_to_x(self.tct.T)
+        self.dT2dx = self.grad_to_x(self.sol.T)
 
-        self.dTdx0 = self.grad_to_x(self.dT2dx * self.tct.thermal_conductivity)
-        self.dTdx1 = self.dTdx0 - self.dT2dx * (self.diffusive_fluxes * self.tct.cp).sum(dim=1, keepdim=True)
-        self.dTdx2 = self.dTdx1 - (self.tct.h * self.tct.molecular_weights.T * self.tct.wdot).sum(dim=1, keepdim=True)
-        self.dTdx = self.dTdx2/self.m_dot / self.tct.cp_mole.unsqueeze(-1)
+        self.dTdx0 = self.grad_to_x(self.dT2dx * self.sol.thermal_conductivity)
+        self.dTdx1 = self.dTdx0 - self.dT2dx * (self.diffusive_fluxes * self.sol.cp).sum(dim=1, keepdim=True)
+        self.dTdx2 = self.dTdx1 - (self.sol.h * self.sol.molecular_weights.T * self.sol.wdot).sum(dim=1, keepdim=True)
+        self.dTdx = self.dTdx2/self.m_dot / self.sol.cp_mole.unsqueeze(-1)
 
     def update_rhs(self):
         self.rhs = torch.cat((self.dTdx, self.dYdx), dim=1)
