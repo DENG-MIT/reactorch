63e7c0da5bd305753c37d9c7138c737d9a738635
Weiqi Ji <jiweiqi10@gmail.com>
2020-09-06 21:24:15 -0400

    Reformat flame_1d.py
    * add nn.Module
    * add super

--- reactorch/flame_1d.py
+++ reactorch/flame_1d.py
@@ -9,20 +9,20 @@ __email__ = "weiqiji@mit.edu"
 __status__ = "Development"
 
 import torch
-
+from torch import nn
 
 torch.set_default_tensor_type("torch.DoubleTensor")
 
 
-class Flame_1d:
+class Flame_1d(nn.Module):
     def __init__(self, sol, device=None):
-        super().__init__()
-        
+        super(Flame_1d, self).__init__()
+
         if device is None:
             self.device = torch.device('cpu')
         else:
             self.device = device
-        
+
         self.sol = sol
 
     def set_states(self, TPY):
@@ -36,17 +36,22 @@ class Flame_1d:
         self.update_rhs()
 
     def set_x(self, x):
+
         self.x = x
+        self.grid = self.x
 
     def set_mdot(self, m_dot):
+
         self.m_dot = torch.Tensor([m_dot]).to(self.device)
 
     def update_u(self):
+
         self.u = self.m_dot / self.sol.density_mass
 
     def grad_to_x(self, output):
 
         if output.shape[1] == 1:
+
             dydt = torch.autograd.grad(outputs=output.sum(),
                                        inputs=self.x,
                                        retain_graph=True,
@@ -55,7 +60,9 @@ class Flame_1d:
 
         else:
             dydt = torch.zeros_like(output)
+
             for i in range(output.shape[1]):
+
                 dydt[:, i] = torch.autograd.grad(outputs=output[:, i].sum(),
                                                  inputs=self.x,
                                                  retain_graph=True,
@@ -66,26 +73,41 @@ class Flame_1d:
 
     def update_diffusive_fluxes(self):
         # following https://cantera.org/science/flames.html
-        self.sol.update_transport()
-        self.diffusive_fluxes_star = - self.sol.density_mass * \
-            (self.sol.molecular_weights.T / self.sol.mean_molecular_weight) \
-            * self.sol.mix_diff_coeffs * self.grad_to_x(self.sol.X)
-        self.diffusive_fluxes = self.diffusive_fluxes_star - \
-            self.sol.Y.clone() * self.diffusive_fluxes_star.sum(dim=1, keepdim=True)
+
+        sol = self.sol
+
+        sol.update_transport()
+
+        diffusive_fluxes_star = - (sol.density_mass *
+                                   (sol.molecular_weights.T / sol.mean_molecular_weight) *
+                                   sol.mix_diff_coeffs * self.grad_to_x(sol.X))
+
+        self.diffusive_fluxes = (diffusive_fluxes_star - self.sol.Y.clone()
+                                 * diffusive_fluxes_star.sum(dim=1, keepdim=True))
 
     def update_dYdx(self):
         # following https://cantera.org/science/flames.html
-        self.dYdx = (-self.grad_to_x(self.diffusive_fluxes) + self.sol.molecular_weights.T * self.sol.wdot) / self.m_dot
+
+        self.dYdx = (- self.grad_to_x(self.diffusive_fluxes) +
+                     self.sol.molecular_weights.T * self.sol.wdot) / self.m_dot
 
     def update_dTdx(self):
         # following https://cantera.org/science/flames.html
-        self.dT2dx = self.grad_to_x(self.sol.T)
 
-        self.dTdx0 = self.grad_to_x(self.dT2dx * self.sol.thermal_conductivity)
-        self.dTdx1 = self.dTdx0 - self.dT2dx * (self.diffusive_fluxes * self.sol.cp / self.sol.molecular_weights.T).sum(dim=1, keepdim=True)
-        self.dTdx2 = self.dTdx1 - (self.sol.h * self.sol.molecular_weights.T * self.sol.wdot).sum(dim=1, keepdim=True)
-        self.dTdx = self.dTdx2/self.m_dot / self.sol.cp_mass.T
+        sol = self.sol
+
+        dT2dx = self.grad_to_x(sol.T)
+
+        dTdx0 = self.grad_to_x(dT2dx * sol.thermal_conductivity)
+
+        MW = sol.molecular_weights.T
+
+        dTdx1 = (dTdx0 - dT2dx * (self.diffusive_fluxes * sol.cp / MW).sum(dim=1, keepdim=True))
+
+        dTdx2 = dTdx1 - (sol.h * MW * sol.wdot).sum(dim=1, keepdim=True)
+
+        self.dTdx = dTdx2 / self.m_dot / sol.cp_mass.T
 
     def update_rhs(self):
-        self.rhs = torch.cat((self.dTdx, self.dYdx), dim=1)
 
+        self.rhs = torch.cat((self.dTdx, self.dYdx), dim=1)
