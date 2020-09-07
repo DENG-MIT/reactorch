c7f8c78f6aa8e1bf9121e4c2ecb1ea3d9b0ce19c
johnstein <jonhnstein@qq.com>
2020-09-07 12:26:03 -0400

    fix bug in viscosities_func by clone X to avoid inplace

--- reactorch/transport.py
+++ reactorch/transport.py
@@ -11,7 +11,7 @@ def update_transport(self):
 
 
 def viscosities_func(self):
-
+    #TODO: investigate why we have to clone X in order to avoid inplace operation
     self.trans_T = torch.cat([self.logT ** i for i in reversed(range(self.poly_order))], dim=1)
 
     self.species_viscosities = torch.mm(self.trans_T, self.species_viscosities_poly)
@@ -27,18 +27,19 @@ def viscosities_func(self):
     self.PHI = (1 / 2.8284271247461903 / torch.sqrt(1 + self.Wk_over_Wj) *
                 (1 + self.etak_over_etaj ** 0.5 * self.Wj_over_Wk ** 0.25) ** 2)
 
-    self.viscosities = (self.X.clone() * self.species_viscosities /
-                        torch.bmm(self.PHI, self.X.unsqueeze(-1)).squeeze(-1)).sum(dim=1,
+    X = self.X.clone()
+    self.viscosities = (X * self.species_viscosities /
+                        torch.bmm(self.PHI, X.unsqueeze(-1)).squeeze(-1)).sum(dim=1,
                                                                                    keepdim=True)
 
 
 def thermal_conductivity_func(self):
-
+    #TODO: investigate why we have to clone X in order to avoid inplace operation
     self.species_thermal_conductivity = torch.mm(self.trans_T, self.thermal_conductivity_poly)
-
+    X = self.X.clone()
     self.thermal_conductivity = 0.5 * (
-        (self.X.clone() * self.species_thermal_conductivity).sum(dim=1, keepdim=True) +
-        1 / (self.X.clone() / self.species_thermal_conductivity).sum(dim=1, keepdim=True)
+        (X * self.species_thermal_conductivity).sum(dim=1, keepdim=True) +
+        1 / (X / self.species_thermal_conductivity).sum(dim=1, keepdim=True)
     )
 
 
